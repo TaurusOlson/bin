d1daa683edad5dd1bbec50e0f2bb1136935d3a81
TaurusOlson <taurusolson@gmail.com>
2010-08-11 12:09:16 +0200

    Modified average.py and added rms.py

--- rms.py
+++ rms.py
@@ -5,32 +5,32 @@
 # Description   : Print the average of the input numbers
 #
 # Usage: 
-# average.py x1 x2 x3 ...
+# rms.py x1 x2 x3 ...
 #
-# average.py <Enter>
+# rms.py <Enter>
 # Then type the values of x_i
 #
-# average.py `awk '{print $1}' file.dat`
+# rms.py `awk '{print $1}' file.dat`
 #--------------------------------------------------------
 
-import sys
+import sys, math
 
-def get_average(x):
-    """Return the average of the x_i"""
+def get_rms(x):
+    """Return the Root Mean Square of the x_i"""
 
     sum = 0
     n = len(x) - 1
     for x_i in x:
         if x_i != x[0]:
-            sum = float(sum) + float(x_i)
+            sum = float(sum) + float(x_i)**2
 
-    return sum/n 
+    return math.sqrt(sum)/n 
+
 
 if len(sys.argv) > 1:
-    print "%f" % get_average(sys.argv)
+    print "%f" % get_rms(sys.argv)
 else:
     x_i = raw_input('Enter the x_i: ')
     a = ['']
     a.extend(list(x_i.split(' ')))
-    print "%f" % get_average(a)
-
+    print "%f" % get_rms(a)
